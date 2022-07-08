591af527faf8045bff8f2b1cbf09815d7caf3765
RCCSDTech <86610455+Jerrod-RCTech@users.noreply.github.com>
2022-07-08 07:58:12 -0400

    Add files via upload
    TitanPOS Kiosk App 0.1

---
+++ config.js
@@ -0,0 +1,8 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Titan POS",
+   "homepage": "https://pos.titank12.com/login",
+   "enableNavBttns": true,
+   "kioskEnabled": true
+};
